51ebbc5d5c4f3751dab8533587781ded90dd8f33
Jeff Minbiole <minbiole.jeff@gmail.com>
2016-08-10 22:55:19 -0500

    fixed styling and working on dynamic ids

---
+++ app/controllers/run-info.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions:{
+    runInfo() {
+      return fetch('http://tiny-tn.herokuapp.com/collections/runs-jm', {
+        method: 'get',
+        body: JSON.stringify({
+          time: this.time,
+          date: this.date,
+          notes: this.notes
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: 'application/json',
+        }
+      })
+        .then((res) => res.json());
+    }
+  }
+});
--- app/routes/run-info.js
+++ app/routes/run-info.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  model() {
+    return fetch(`http://tiny-tn.herokuapp.com/collections/runs-jm`)
+      .then((res) => res.json());
+  }
 });
